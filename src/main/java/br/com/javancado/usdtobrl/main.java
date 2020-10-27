a18b1031d6e0c8ed59fed25ebc50625db44bdf50
Thiago Almeida <thiagolimadealmeid4@gmail.com>
2020-10-27 12:15:00 -0300

    corrections

--- src/main/java/br/com/javancado/usdtobrl/main.java
+++ src/main/java/br/com/javancado/usdtobrl/main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConversorApplication {
+public class main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConversorApplication.class, args);
+		SpringApplication.run(main.class, args);
 	}
 
 }
